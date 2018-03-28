49fdf8ca1c636c6fa505ef9a1eecd7df01c50b12
Josiah Pang <pang.josiah@gmail.com>
2018-03-28 15:46:59 -0700

    add .gitignore

---
+++ read.py
@@ -0,0 +1,28 @@
+# Project: Google Cloud Credit Code Reader
+# Author: Josiah Pang
+# Spring 2018
+# This program takes images of GCP Credits and outputs their individual unique
+# codes. It was written to help Student Innovators track credit activations.
+
+import config
+
+def detect_text(path):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
