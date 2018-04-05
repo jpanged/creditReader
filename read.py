a85b6274a23d2591997fd97e8c4993a42c1f1e03
Josiah Pang <pang.josiah@gmail.com>
2018-04-04 18:43:39 -0700

    allow local files

--- read.py
+++ read.py
@@ -3,43 +3,33 @@
 # Spring 2018
 # This program takes images of GCP Credits and outputs their individual unique
 # codes. It was written to help Student Innovators track credit activations.
+# It can either take local images or images hosted on a Google Cloud Storage
+# Bucket. The program returns an 'out.txt' file with the codes separated by a
+# new line in addition to printing to the console. Store local images in the
+# "/images" folder.
 
 # Import libraries
 import config
 import io
 import os
 import re
+from sys import *
 
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
 
-def main():
-    detect_text_uri('gs://' + config.CLOUD_STORAGE_BUCKET + '/credits-04.jpg')
+def main(): # Uncomment the function you want to use (local vs cloud storage)
+    try:
+        # For local images
+        img_path = os.path.join(path[0], 'images/' + argv[1])
+        detect_text(img_path)
 
-# For label detection
-def detect(labels):
-    # Instantiates a client
-    client = vision.ImageAnnotatorClient()
+        # For images hosted on Cloud Storage Bucket
+        #detect_text_uri('gs://' + config.CLOUD_STORAGE_BUCKET + '/' + argv[1])
+    except:
+        print("Usage: python read.py IMAGE_NAME")
 
-    # The name of the image file to annotate
-    file_name = os.path.join(
-        os.path.dirname(__file__),
-        'images/credits-02.jpg')
-
-    # Loads the image into memory
-    with io.open(file_name, 'rb') as image_file:
-        content = image_file.read()
-
-    image = types.Image(content=content)
-
-    # Performs label detection on the image file
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
-
-    print('Labels:')
-    for label in labels:
-        print(label.description)
 
 # For remote images
 def detect_text_uri(uri):
@@ -81,15 +71,22 @@ def detect_text(path):
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    print('Texts:')
 
+    # Prep file for writing
+    outFile = open('out.txt', 'w+')
+
+    # Traverse detected text and only save valid codes
+    prev = ''
+    pattern = '[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]-[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]-[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]-[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]'
     for text in texts:
-        print('\n"{}"'.format(text.description))
+        cur = '{}'.format(text.description)
+        if re.match(pattern, cur): # Use regex to identify code
+            print(cur)
+            outFile.write(cur + '\n')
+        prev = cur
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
-
-        print('bounds: {}'.format(','.join(vertices)))
+    # Close written file
+    outFile.close()
 
 if __name__ == '__main__':
     main()
